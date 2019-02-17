f92995e40f5e7f185f2042eb784a2edc84074548
VeronikaRahuba <47725151+VeronikaRahuba@users.noreply.github.com>
2019-02-17 23:59:06 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        short n=2;
+        short m=2;
+        short a=1;
+        short b=2;
+        short C=0;
+        float r=0;
+        float r1=0;
+        for(short i=a; i<=n; i++)
+        {
+            for(short j=b; j<=m; j++)
+            {
+                r+=((float)i/(float)j)/(i-C);
+            }
+        }
+        System.out.println("r= " + r);
+    }
+}

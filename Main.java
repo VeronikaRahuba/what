b959f43a5cbd67fe96b067f85595cb5d35d38611
VeronikaRahuba <47725151+VeronikaRahuba@users.noreply.github.com>
2019-02-18 22:20:13 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,11 +8,15 @@ public class Main
         short b=2;
         short C=0;
         float r=0;
-        float r1=0;
         for(short i=a; i<=n; i++)
         {
             for(short j=b; j<=m; j++)
             {
+                if((i-C)==0) 
+                {
+                    System.out.println("Error");
+                    System.exit(0);
+                }
                 r+=((float)i/(float)j)/(i-C);
             }
         }

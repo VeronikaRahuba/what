57ca910ac867c4c910ee221db1f975ae63754e04
VeronikaRahuba <47725151+VeronikaRahuba@users.noreply.github.com>
2019-02-26 00:20:16 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,25 +1,63 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        short n=2;
-        short m=2;
-        short a=1;
-        short b=2;
-        short C=0;
-        float r=0;
-        for(short i=a; i<=n; i++)
-        {
-            for(short j=b; j<=m; j++)
-            {
-                if(((i-C)==0) || (j==o))
-                {
-                    System.out.println("Error");
-                    System.exit(0);
-                }
-                r+=((float)i/(float)j)/(i-C);
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        byte B[][];
+        byte C[][];
+        byte s;
+        byte c;
+        byte a;
+        byte b;
+        byte i;
+        byte j;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Количество строк");
+        s = scanner.nextByte();
+        System.out.println("Количество столбиков");
+        c = scanner.nextByte();
+        B = new byte[s][c];
+        C = new byte[c][s];
+        System.out.println("Ввод поэлементно");
+        for (i = 0; i < s; i++) {
+            for (j = 0; j < c; j++) {
+                B[i][j] = scanner.nextByte();
             }
         }
-        System.out.println("r= " + r);
+        System.out.println("Ваша матрица В:");
+        for (i = 0; i < s; i++) {
+            for (j = 0; j < c; j++) {
+                System.out.print("  " + B[i][j]);
+            }
+            System.out.println();
+        }
+
+        for (i = 0; i < s; i++) {
+            for (j = 0; j < c; j++) {
+                C[j][i] = B[i][j];
+            }
+
+        }
+
+        System.out.println("Транмпонированная матрица С:");
+        for (j = 0; j < c; j++) {
+            for (i = 0; i < s; i++) {
+                System.out.print("  " + C[j][i]);
+            }
+            System.out.println();
+        }
+
+        b = 0;
+        for (i = 0; i < s; i++) {
+            a = C[0][i];
+            for (j = 0; j < c-1; j++)
+            {
+                if (a <  C[j+1][i])
+                {
+                    a=C[j+1][i];
+                }
+            }
+            b += a;
+        }
+        System.out.print("Сумма макс.элем в столбиках = " + b);
     }
 }
